9c3923b6cc0d79076b43f5cc30446a1c6eb44222
Lorenz Hankel <lorenz.hankel@yahoo.de>
2022-12-12 13:02:50 +0100

    updated

--- Zutatart.py
+++ Zutatart.py
@@ -1,7 +1,19 @@
+# LH & GD
 class Zutatart:
     def __init__(self,bezeichner:str = "Beispiel" , preis:float =0.0, bestand:int = 0 ):
         self.__bezeichner = bezeichner
         self.__preis = preis
         self.__bestand = bestand
     
+    
+    # LH Setter 
+    def set_bezeichner(self, bezeichner:str):
+        if bezeichner.len() > 0:
+            self.__bezeichner = bezeichner
+        else:
+            print("Bezeichnung ist zu kurz. Bitte eine längere wählen.")
+
+    # LH Getter
+    def get_bezeichner(self):
+        return self.__bezeichner
     
