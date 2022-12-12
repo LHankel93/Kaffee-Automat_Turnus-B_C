4d4c8c38e66fde831f518e86f357e6152a22127e
Lorenz Hankel <lorenz.hankel@yahoo.de>
2022-12-12 12:43:35 +0100

    updated

---
+++ KaffeeautomatSim.py
@@ -0,0 +1,13 @@
+
+# import <Dateiname ohne Endung>
+# Aufruf von fremder Datei-Methode mit <Dateiname Ohne Endung>.<Methodenname>
+# LH Ja moin. Dies ist die Main Klasse und simuliert einen Kaffee-Automaten.
+# Main Methode heißt "kaffeeautomat"
+
+def klassen_initialisieren():
+    # LH von klein nach große Klassen initialisieren / deklarieren
+    pass
+
+def kaffeeautomat():
+    klassen_initialisieren()
+
---
+++ test.py
@@ -0,0 +1,2 @@
+def uwu():
+    print("UwU")
