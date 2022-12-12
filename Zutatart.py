7294ed208ff80fe34b1f77f3fc32c09b5d093e2e
Lorenz Hankel <lorenz.hankel@yahoo.de>
2022-12-12 12:49:45 +0100

    updated

---
+++ Zutatart.py
@@ -0,0 +1,7 @@
+class Zutatart:
+    def __init__(self,bezeichner:str = "Beispiel" , preis:float =0.0, bestand:int = 0 ):
+        self.__bezeichner = bezeichner
+        self.__preis = preis
+        self.__bestand = bestand
+    
+    
